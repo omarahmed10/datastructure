4d0ee555b8e245c2742cf4573b6f49167cefc7fd
omarahmed <omarahmed@omarahmed-HP-ProBook-450-G2>
2016-03-13 13:09:54 +0200

    new proj 3

---
+++ FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
@@ -0,0 +1,20 @@
+package eg.edu.alexu.csd.datastructure.iceHockey.cs47;
+
+import java.awt.Point;
+
+import eg.edu.alexu.csd.datastructure.iceHockey.IPlayersFinder;
+
+public class PlayGround implements IPlayersFinder {
+
+	@Override
+	public Point[] findPlayers(String[] photo, int team, int threshold) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
