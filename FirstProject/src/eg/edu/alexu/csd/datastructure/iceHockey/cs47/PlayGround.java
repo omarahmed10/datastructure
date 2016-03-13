ccce5d470ed74774f53845873db443006f75575e
omarahmed <omarahmed@omarahmed-HP-ProBook-450-G2>
2016-03-13 18:18:08 +0200

    frist implementation

--- FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
+++ FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
@@ -5,16 +5,56 @@ import java.awt.Point;
 import eg.edu.alexu.csd.datastructure.iceHockey.IPlayersFinder;
 
 public class PlayGround implements IPlayersFinder {
-
+	
+	final int max = 51;
+	int cnt = 0;
+	int team;
+	int threshold;
+	String[] maze =new String[max];
+	boolean[][] vis =new boolean[max][max];
+	
 	@Override
 	public Point[] findPlayers(String[] photo, int team, int threshold) {
-		// TODO Auto-generated method stub
+		maze = photo;
+		this.team =team;
+		this.threshold = threshold;
+		cntReachalbleCells( photo.length ,photo[0].length() );
+		
 		return null;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public void cntReachalbleCells(int r,int c)
+	{
+	    if( !valid(r,c) || maze[r].charAt(c) != team || vis[r][c]== true)
+			return;		// invalid position or block position
 
+
+		vis[r][c] = true;	// we just visited it, don't allow any one back to it
+		cnt++;
+	    System.out.println(r+","+c);
+		// Try the 4 neighbor cells
+		cntReachalbleCells(r,c-1);
+		cntReachalbleCells(r,c+1);
+	    cntReachalbleCells(r-1,c);
+	    cntReachalbleCells(r+1,c);
+	}
+	public boolean valid(int r,int c){
+	    if( r<0 || r>3 || c<0 || c>3)
+	        return false;
+	    return true;
+	}
+
+	public static void main(String[] args) {
+		PlayGround pgObject =new PlayGround();
+		
+	    for(int i =0;i<4;i++)
+	        for(int j =0;j<4;j++)
+	    {
+	        pgObject.cnt =0;
+	        pgObject.cntReachalbleCells(i,j);
+	        System.out.println(pgObject.cnt + "\n");
+	    }
 	}
 
 }
