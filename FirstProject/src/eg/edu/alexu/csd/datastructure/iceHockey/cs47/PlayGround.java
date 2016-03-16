d2a377ed2e2ab1aa9a2a24cd1942e97af70df08a
omarahmed <omarahmed@omarahmed-HP-ProBook-450-G2>
2016-03-16 18:54:22 +0200

    almost done

--- FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
+++ FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
@@ -7,34 +7,52 @@ import eg.edu.alexu.csd.datastructure.iceHockey.IPlayersFinder;
 public class PlayGround implements IPlayersFinder {
 
 	final int max = 51;
-	int cnt = 0;
+	int cnt;
 	int team;
 	int threshold;
-	String[] maze ;
+	String[] maze;
 	boolean[][] vis = new boolean[max][max];
 	int[][] playerPosition;
 
 	@Override
 	public Point[] findPlayers(String[] photo, int team, int threshold) {
-		maze =new String[photo.length];
+		maze = new String[photo.length];
 		maze = photo;
-		playerPosition =new int [photo.length * photo[0].length()][2];
+
+		int x = photo.length * photo[0].length(); ///// photo[0].length() is
+													///// length of one string
+													///// as all given string
+													///// are equal in length
+		playerPosition = new int[x][2];
 		this.team = team;
 		this.threshold = threshold;
-		cntReachalbleCells(photo.length, photo[0].length());
+		for (int I = 0; I < photo.length; I++) {
+			for (int J = 0; J < photo[I].length(); J++) {
+				////////// initializing playerPositions list to avoid
+				////////// NullPointer
+				for (int i = 0; i < x; i++)
+					for (int j = 0; j < 2; j++)
+						playerPosition[i][j] = 0;
+				/////////
+				cnt = 0;
+				cntReachalbleCells(photo.length, photo[0].length());
+			}
+		}
 
 		return null;
 	}
 
 	public void cntReachalbleCells(int r, int c) {
-		if (!valid(r, c) 
-				|| maze[r].charAt(c) != team 
-				|| vis[r][c] == true)
-			return; // invalid position or block position
+		if (!valid(r, c) || maze[r].charAt(c) != team || vis[r][c] == true)
+			return; // invalid position
 
 		vis[r][c] = true; // we just visited it, don't allow any one back to it
-		cnt++;
-		System.out.println(r + "," + c);
+
+		playerPosition[cnt][0] = r; ////// saving player position (x coordinate)
+		playerPosition[cnt][1] = c; ////// (y coordinate)
+
+		cnt++; ///// counting number of cell where a player was found
+
 		// Try the 4 neighbor cells
 		cntReachalbleCells(r, c - 1);
 		cntReachalbleCells(r, c + 1);
