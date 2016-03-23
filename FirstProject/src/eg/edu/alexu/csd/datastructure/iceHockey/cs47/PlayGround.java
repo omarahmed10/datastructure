d7e195316ade10320c0e9bd9855c086326dc8e82
omarahmed <omarahmed@omarahmed-HP-ProBook-450-G2>
2016-03-23 22:13:36 +0200

    trick2

--- FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
+++ FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
@@ -11,7 +11,7 @@ public class PlayGround implements IPlayersFinder {
 	private final int max = 51;
 	private int cnt;
 	private int noOfPlayer = 0;
-	private char team ;
+	private char team;
 	private String[] image;
 	private boolean[][] vis = new boolean[max][max];
 	private int[][] playerPosition;
@@ -20,52 +20,50 @@ public class PlayGround implements IPlayersFinder {
 
 	@Override
 	public Point[] findPlayers(String[] photo, int team, int threshold) {
-	
+
 		image = new String[photo.length];
 		image = photo;
-		this.team = (char)(team+48);
+		this.team = (char) (team + 48);
 		i++;
-		if(i>1){
-			Integer o = i;
-			throw new RuntimeException(o.toString());
-			}
-		throw new RuntimeException(image[0]);
-//
-//		if (photo.length != 0) {
-//			int x = photo.length * photo[0].length(); ///// photo[0].length() is
-//														///// length of one
-//														///// string
-//														///// as all given
-//														///// string are equal
-//														///// in length
-//			playerPosition = new int[x][2];
-//			for (int I = 0; I < photo.length; I++) {
-//				for (int J = 0; J < photo[I].length(); J++) {
-//					////////// initializing playerPositions list to avoid
-//					////////// NullPointer
-//					for (int i = 0; i < x; i++)
-//						for (int j = 0; j < 2; j++)
-//							playerPosition[i][j] = 0;
-//					/////////
-//					cnt = 0;
-//					cntReachalbleCells(I, J);
-//					if (cnt * 4 >= threshold) {
-//						positions[noOfPlayer] = new Point(0, 0); ////// initializing
-//						positions[noOfPlayer] = new Point(getPlayerCenter(playerPosition, cnt));
-//						noOfPlayer++;
-//						System.out.println(positions[noOfPlayer]);
-//						System.out.println(noOfPlayer);
-//					}
-//				}
-//			}
-//			Arrays.sort(positions, 0, noOfPlayer, new PointCmp());
-//			Point[] finalPositions = new Point[noOfPlayer];
-//			for (int i = 0; i < noOfPlayer; i++)
-//				finalPositions[i] = positions[i];
-//			return finalPositions;
-//		}
-//		Point[] finalPositions = {};
-//		return finalPositions;
+		Integer o = i;
+		throw new RuntimeException(o.toString());
+		//
+		// if (photo.length != 0) {
+		// int x = photo.length * photo[0].length(); ///// photo[0].length() is
+		// ///// length of one
+		// ///// string
+		// ///// as all given
+		// ///// string are equal
+		// ///// in length
+		// playerPosition = new int[x][2];
+		// for (int I = 0; I < photo.length; I++) {
+		// for (int J = 0; J < photo[I].length(); J++) {
+		// ////////// initializing playerPositions list to avoid
+		// ////////// NullPointer
+		// for (int i = 0; i < x; i++)
+		// for (int j = 0; j < 2; j++)
+		// playerPosition[i][j] = 0;
+		// /////////
+		// cnt = 0;
+		// cntReachalbleCells(I, J);
+		// if (cnt * 4 >= threshold) {
+		// positions[noOfPlayer] = new Point(0, 0); ////// initializing
+		// positions[noOfPlayer] = new Point(getPlayerCenter(playerPosition,
+		// cnt));
+		// noOfPlayer++;
+		// System.out.println(positions[noOfPlayer]);
+		// System.out.println(noOfPlayer);
+		// }
+		// }
+		// }
+		// Arrays.sort(positions, 0, noOfPlayer, new PointCmp());
+		// Point[] finalPositions = new Point[noOfPlayer];
+		// for (int i = 0; i < noOfPlayer; i++)
+		// finalPositions[i] = positions[i];
+		// return finalPositions;
+		// }
+		// Point[] finalPositions = {};
+		// return finalPositions;
 	}
 
 	public void cntReachalbleCells(int r, int c) {
@@ -75,10 +73,10 @@ public class PlayGround implements IPlayersFinder {
 		vis[r][c] = true; // we just visited it, don't allow any one back to it
 
 		if (image[r].charAt(c) == team) {
-			playerPosition[cnt][0] = c; ////// saving player position (c
-										////// coordinate)
-			playerPosition[cnt][1] = r; ////// (y coordinate)
-			cnt++; ///// counting number of cell where a player was found
+			playerPosition[cnt][0] = c; // //// saving player position (c
+										// //// coordinate)
+			playerPosition[cnt][1] = r; // //// (y coordinate)
+			cnt++; // /// counting number of cell where a player was found
 		}
 		cntReachalbleCells(r, c - 1);
 		cntReachalbleCells(r, c + 1);
@@ -119,11 +117,11 @@ public class PlayGround implements IPlayersFinder {
 		PlayGround pgObject = new PlayGround();
 		Point[] answer = pgObject.findPlayers(image, 1, 4);
 		String[] image1 = { "3", "3", "3" };
-		
+
 		for (int i = 0; i < answer.length; i++)
 			if (answer[i] != null)
 				System.out.println(answer[i].x + "," + answer[i].y);
-		
+
 		Point[] answer1 = pgObject.findPlayers(image1, 3, 3);
 		for (int i = 0; i < answer1.length; i++)
 			if (answer1[i] != null)
