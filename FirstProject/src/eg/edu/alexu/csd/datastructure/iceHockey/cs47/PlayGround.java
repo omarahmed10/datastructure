493bfc8e4f15ce70ed44bbe14e6cf431e9070a45
omarahmed <omarahmed@omarahmed-HP-ProBook-450-G2>
2016-03-24 21:54:48 +0200

    sdsdsd

--- FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
+++ FirstProject/src/eg/edu/alexu/csd/datastructure/iceHockey/cs47/PlayGround.java
@@ -35,37 +35,36 @@ public class PlayGround implements IPlayersFinder {
 		image = new String[photo.length+1];
 		image = photo;
 //		System.out.println(photo.length);
-		noOfPlayer = 0;
+//		noOfPlayer = 0;
 		this.team = (char) (team + 48);
 		if (photo.length != 0) {
 			int x = photo.length * photo[0].length();  	// photo[0].length()
 														// is length of one string 
 														// as all given string are equal in length
-			throw new RuntimeException("hello");
-//			playerPosition = new int[x][2];
-//			for (int I = 0; I < photo.length; I++) {
-//				for (int J = 0; J < photo[I].length(); J++) {
-//					// //////// initializing playerPositions list to avoid
-//					// //////// NullPointer
-//					for (int i = 0; i < x; i++)
-//						for (int j = 0; j < 2; j++)
-//							playerPosition[i][j] = 0;
-//					// ///////
-//					cnt = 0;
-//					cntReachalbleCells(I, J);
-//					if (cnt * 4 >= threshold) {
-//						positions[noOfPlayer] = new Point(0, 0); // initializing
-//						positions[noOfPlayer] = new Point(getPlayerCenter(playerPosition, cnt));
-//						noOfPlayer++;
-//					}
-//				}
-//			}
-//			Arrays.sort(positions, 0, noOfPlayer, new PointCmp());
-//			finalPositions = new Point[noOfPlayer];
-////			Point[] finalPositions = new Point[noOfPlayer];
-//			for (int i = 0; i < noOfPlayer; i++)
-//				finalPositions[i] = positions[i];
-//			return finalPositions;
+			playerPosition = new int[x][2];
+			for (int I = 0; I < photo.length; I++) {
+				for (int J = 0; J < photo[I].length(); J++) {
+					// //////// initializing playerPositions list to avoid
+					// //////// NullPointer
+					for (int i = 0; i < x; i++)
+						for (int j = 0; j < 2; j++)
+							playerPosition[i][j] = 0;
+					// ///////
+					cnt = 0;
+					cntReachalbleCells(I, J);
+					if (cnt * 4 >= threshold) {
+						positions[noOfPlayer] = new Point(0, 0); // initializing
+						positions[noOfPlayer] = new Point(getPlayerCenter(playerPosition, cnt));
+						noOfPlayer++;
+					}
+				}
+			}
+			Arrays.sort(positions, 0, noOfPlayer, new PointCmp());
+			finalPositions = new Point[noOfPlayer];
+//			Point[] finalPositions = new Point[noOfPlayer];
+			for (int i = 0; i < noOfPlayer; i++)
+				finalPositions[i] = positions[i];
+			return finalPositions;
 		}
 		finalPositions = new Point[]{};
 		return finalPositions;
