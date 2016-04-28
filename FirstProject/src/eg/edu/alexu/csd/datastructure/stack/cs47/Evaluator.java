c5286397923084aa389ce7bffbd4e1b7810f0025
omarahmed <omarahmed@omarahmed-HP-ProBook-450-G2>
2016-04-28 21:25:54 +0200

    i think it is perfect

--- FirstProject/src/eg/edu/alexu/csd/datastructure/stack/cs47/Evaluator.java
+++ FirstProject/src/eg/edu/alexu/csd/datastructure/stack/cs47/Evaluator.java
@@ -15,62 +15,65 @@ public class Evaluator implements IExpressionEvaluator {
 
 	@Override
 	public final String infixToPostfix(final String expression) {
-		String ali = expression.replaceAll("\\s+", "");
-		if (ali.length() == 0) {
+		if (expression.length() == 0 || expression == null) {
 			throw new RuntimeException();
 		}
-		String regex = "(?<=[-+*/()])|(?=[-+*/()])";
-		String[] omar = ali.split(regex);
 		StringBuilder infix = new StringBuilder();
 		int noOperator = 0;
 		int noOperand = 0;
-		for (int j = 0; j < omar.length; j++) {
+		for (int j = 0; j < expression.length(); j++) {
 			boolean printSpace = false;
-			if (omar[j] == null || omar[j].isEmpty()) { // ignore it
+			char p = expression.charAt(j);
+			if (p == ' ') {
 				continue;
-			}
-			try {
-				infix.append(Integer.parseInt(omar[j]));
+			} else if (Character.isDigit(p)) {
+				int number = 0;
+				number = Character.getNumericValue(p);
+				while (j < expression.length() - 1
+						&& Character.isDigit(expression.charAt(j + 1))) {
+					number = number * 10 + Character
+							.getNumericValue(expression.charAt(j + 1));
+					j++;
+				}
+				infix.append(Character.getNumericValue(p));
 				noOperand++;
 				printSpace = true;
-			} catch (Exception e) {
-				char p = omar[j].charAt(0);
+			}
 
-				if (p == '-' || p == '+') {
-					if (infix.length() == 0) {
-						throw new RuntimeException();
-					}
-					noOperator++;
-					while (exp.size() > 0 && !exp.peek().equals('(')) {
-						infix.append(exp.pop() + " ");
-					}
-					exp.push(p);
-				} else if (p == '*' || p == '/') {
-					if (infix.length() == 0) {
-						throw new RuntimeException();
-					}
-					noOperator++;
-					while (exp.size() > 0 && (exp.peek().equals('*')
-							|| exp.peek().equals('/'))) {
-						infix.append(exp.pop() + " ");
-					}
-					exp.push(p);
-				} else if (p == '(') {
-					exp.push(p);
-				} else if (p == ')') {
-					while (exp.size() > 0 && !exp.peek().equals('(')) {
-						infix.append(exp.pop() + " ");
-					}
-					if (exp.size() > 0 && exp.peek().equals('(')) {
-						exp.pop();
-					} else { // there isn't an opening bracket wrong expression
-						throw new RuntimeException();
-					}
-				} else {
-					noOperand++;
-					printSpace = true;
-					infix.append(p);
+			else if (p == '-' || p == '+') {
+				if (infix.length() == 0) {
+					throw new RuntimeException();
 				}
+				noOperator++;
+				while (exp.size() > 0 && !exp.peek().equals('(')) {
+					infix.append(exp.pop() + " ");
+				}
+				exp.push(p);
+			} else if (p == '*' || p == '/') {
+				if (infix.length() == 0) {
+					throw new RuntimeException();
+				}
+				noOperator++;
+				while (exp.size() > 0
+						&& (exp.peek().equals('*') || exp.peek().equals('/'))) {
+					infix.append(exp.pop() + " ");
+				}
+				exp.push(p);
+			} else if (p == '(') {
+				exp.push(p);
+			} else if (p == ')') {
+				while (exp.size() > 0 && !exp.peek().equals('(')) {
+					infix.append(exp.pop() + " ");
+				}
+				if (exp.size() > 0 && exp.peek().equals('(')) {
+					exp.pop();
+				} else { // there isn't an opening bracket wrong expression
+					throw new RuntimeException();
+				}
+			} else {
+				noOperand++;
+				printSpace = true;
+				infix.append(p);
 			}
 			if (printSpace) {
 				infix.append(" ");
@@ -94,39 +97,44 @@ public class Evaluator implements IExpressionEvaluator {
 //		 System.out.println(infix);
 		return infix.toString();
 	}
+
 	@Override
 	public final int evaluate(final String expression) {
 		if (expression.length() == 0) {
 			throw new RuntimeException();
 		}
-		String regex = "\\s";
-		String[] omar = expression.split(regex);
-		for (int i = 0; i < omar.length; i++) {
-			try {
-				/*
-				 * checking unary
-				 */
-				Integer.parseInt(omar[i]);
-				if (i != omar.length - 1 && (omar[i].charAt(0) == '-'
-						|| omar[i].charAt(0) == '+')) {
-					throw new RuntimeException();
-				} else {
-					exp.push(Float.parseFloat(omar[i]));
+		for (int i = 0; i < expression.length(); i++) {
+			char p = expression.charAt(i);
+			if (p == ' ') {
+				continue;
+			} else if (Character.isDigit(p)) {
+				int number = 0;
+				number = Character.getNumericValue(p);
+				while (i < expression.length() - 1
+						&& Character.isDigit(expression.charAt(i + 1))) {
+					number = number * 10 + Character
+							.getNumericValue(expression.charAt(i + 1));
+					i++;
 				}
-			} catch (Exception e) {
+				exp.push((float) number);
+			} else {
+				if (i < expression.length() - 1
+						&& Character.isDigit(expression.charAt(i + 1))) {
+					throw new RuntimeException();
+				}
 				float x = (float) exp.pop();
 				float y = (float) exp.pop();
-				switch (omar[i]) {
-				case "/":
+				switch (p) {
+				case '/':
 					exp.push(y / x);
 					break;
-				case "*":
+				case '*':
 					exp.push(x * y);
 					break;
-				case "+":
+				case '+':
 					exp.push(x + y);
 					break;
-				case "-":
+				case '-':
 					exp.push(y - x);
 					break;
 				default:
@@ -138,7 +146,7 @@ public class Evaluator implements IExpressionEvaluator {
 			throw new RuntimeException();
 		}
 		float answer = (float) exp.pop();
-		// System.out.println(answer);
+//		 System.out.println(answer);
 		return (int) answer;
 	}
 
@@ -148,21 +156,23 @@ public class Evaluator implements IExpressionEvaluator {
 	 * 
 	 */
 	public static void main(String[] args) {
-//	    IExpressionEvaluator testMe = new Evaluator();
-//	    StringBuilder string = new StringBuilder();
-//	 
-//	    for (int i = 0; i < 10000000; i++) {
-//	        string = string.append("1+");
-//	    }
-//	    string = string.append("1");
-//	    long start = System.currentTimeMillis();
-//	    String ev = testMe.infixToPostfix(string.toString());
-//	    long first = System.currentTimeMillis();
-//	    testMe.evaluate(ev);
-//	    long second = System.currentTimeMillis();
-//	    System.out.println(first-start);
-//	    System.out.println(second-first);
-//	    System.out.println(second-start);
-	    }
+//		IExpressionEvaluator testMe = new Evaluator();
+//		StringBuilder string = new StringBuilder();
+//		testMe.evaluate("100 33 - 111 +");
+//		testMe.infixToPostfix("(a / (b - c + d)) * (e - a) * c");
+//		testMe.infixToPostfix("a/b-c+d*e-a*c");
+//		for (int i = 0; i < 1000000; i++) {
+//			string = string.append("1+");
+//		}
+//		string = string.append("1");
+//		long start = System.currentTimeMillis();
+//		String ev = testMe.infixToPostfix(string.toString());
+//		long first = System.currentTimeMillis();
+//		testMe.evaluate(ev);
+//		long second = System.currentTimeMillis();
+//		System.out.println(first - start);
+//		System.out.println(second - first);
+//		System.out.println(second - start);
+	}
 
 }
